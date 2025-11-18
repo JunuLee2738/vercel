9f4414fbaf21c153c85eabbfc4e31d0bb31f1844
JunuLee2738 <ijunu2738@gmail.com>
2025-11-18 15:21:55 +0900

    Update image.js

--- api/image.js
+++ api/image.js
@@ -1,19 +1,5 @@
-import { get } from "@vercel/blob";
-
-export default async function handler(req, res) {
-  const { id } = req.query;
-  if (!id) return res.status(400).send("Missing id");
-
-  try {
-    const blob = await get(`images/${id}`);
-    const arrayBuffer = await blob.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
-
-    res.setHeader("Content-Type", blob.contentType);
-    res.setHeader("Content-Length", buffer.length);
-
-    return res.status(200).send(buffer);
-  } catch (e) {
-    return res.status(404).send("File not found");
-  }
+export default function handler(req, res) {
+  const { id, name } = req.query;
+  if (!id || !name) return res.status(400).send("Missing query parameters");
+  return res.redirect(`https://i.ibb.co/${id}/${name}`);
 }
