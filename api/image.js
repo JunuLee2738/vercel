5e6058fc5330cffb36189d95110fabb7094e2762
JunuLee2738 <ijunu2738@gmail.com>
2025-11-18 15:09:54 +0900

    Update image.js

--- api/image.js
+++ api/image.js
@@ -2,20 +2,18 @@ import { get } from "@vercel/blob";
 
 export default async function handler(req, res) {
   const { id } = req.query;
-
   if (!id) return res.status(400).send("Missing id");
 
-  const filePath = `images/${id}`;
-
   try {
-    const blob = await get(filePath);
+    const blob = await get(`images/${id}`);
     const arrayBuffer = await blob.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
 
     res.setHeader("Content-Type", blob.contentType);
     res.setHeader("Content-Length", buffer.length);
+
     return res.status(200).send(buffer);
-  } catch (err) {
+  } catch (e) {
     return res.status(404).send("File not found");
   }
 }
